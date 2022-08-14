bf9fad336b773429e81313ca2b0d8a6e83a76c73
murad <muradkv@gmail.com>
2022-08-15 02:45:20 +0300

    Create Restaurant data Model

---
+++ FoodPin3/Model/Restaurant.swift
@@ -0,0 +1,38 @@
+//
+//  Restaurant.swift
+//  FoodPin3
+//
+//  Created by murad on 15.08.2022.
+//
+
+import Foundation
+
+struct Restaurant {    
+    let names: String
+}
+
+struct RestaurantList {
+    static let allValues = [
+        Restaurant(names: "Cafe Deadend"),
+        Restaurant(names: "Homei"),
+        Restaurant(names: "Teakha"),
+        Restaurant(names: "Cafe Loisl"),
+        Restaurant(names: "Petite Oyster"),
+        Restaurant(names: "For Kee Restaurant"),
+        Restaurant(names: "Po's Atelier"),
+        Restaurant(names: "Bourke Street Bakery"),
+        Restaurant(names: "Haigh's Chocolate"),
+        Restaurant(names: "Palomino Espresso"),
+        Restaurant(names: "Upstate"),
+        Restaurant(names: "Traif"),
+        Restaurant(names: "Graham Avenue Meats And Deli"),
+        Restaurant(names: "Waffle & Wolf"),
+        Restaurant(names: "Five Leaves"),
+        Restaurant(names: "Cafe Lore"),
+        Restaurant(names: "Confessional"),
+        Restaurant(names: "Barrafina"),
+        Restaurant(names: "Donostia"),
+        Restaurant(names: "Royal Oak"),
+        Restaurant(names: "CASK Pub and Kitchen")
+    ]
+}
