7566b6a9eab83b951d35089e5749c3ec448952ba
murad <muradkv@gmail.com>
2022-08-15 02:59:02 +0300

    Show restaurant names in table

--- FoodPin3/Controller/TableViewController.swift
+++ FoodPin3/Controller/TableViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    let restaurants = RestaurantList.allValues
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,12 +20,19 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return restaurants.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dataCell", for: indexPath)
+        
+        cell.textLabel?.text = restaurants[indexPath.row].names
+        return cell
     }
 
 }
