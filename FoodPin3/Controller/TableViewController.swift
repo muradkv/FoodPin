089b97da4e5f59cd746ca5880cb61d75c0311d6e
murad <muradkv@gmail.com>
2022-08-14 21:20:36 +0300

    Initial commit

---
+++ FoodPin3/Controller/TableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewController.swift
+//  FoodPin3
+//
+//  Created by murad on 14.08.2022.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
