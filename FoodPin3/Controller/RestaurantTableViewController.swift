6c6f9972fd68a60b0c143ea02f87bd1f734f4586
murad <muradkv@gmail.com>
2022-08-23 18:20:43 +0300

    Create another UI with new cell (favoriteCell)

--- FoodPin3/Controller/RestaurantTableViewController.swift
+++ FoodPin3/Controller/RestaurantTableViewController.swift
@@ -30,7 +30,7 @@ class RestaurantTableViewController: UITableViewController {
 
     func configureDataSource() -> UITableViewDiffableDataSource<Section, Restaurant> {
         
-        let cellIdentifier = "dataCell"
+        let cellIdentifier = "favoriteCell"
         
         let dataSource = UITableViewDiffableDataSource<Section, Restaurant> (
             tableView: tableView,
